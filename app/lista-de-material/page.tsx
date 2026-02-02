fbdd8286246c80853656c243608a4dcf81c0aec7
João Vaz <joaomarcelospmvaz@gmail.com>
2026-02-02 14:03:55 -0300

    feat: updating materials

--- app/lista-de-material/page.tsx
+++ app/lista-de-material/page.tsx
@@ -6,34 +6,34 @@ export default function ListaDeMaterial() {
           <span className="text-white ml-1 text-center">Lista de Material</span> <span className="text-white text-4xl bg-[#204111] -rotate-3 mt-2 px-1 xs:-mt-2 mx-5 xs:mx-0">2026</span>
         </p>
       </div>
-      <p className="text-3xl text-[#254a15] font-bold text-center mt-16">Em breve</p>
-      {/* <p className="text-3xl text-[#254a15] font-bold text-center mt-16">Clique abaixo para visualizar</p> */}
+      {/* <p className="text-3xl text-[#254a15] font-bold text-center mt-16">Em breve</p> */}
+      <p className="text-3xl text-[#254a15] font-bold text-center mt-16">Clique abaixo para visualizar</p>
       <div className="my-12 flex flex-col items-center justify-center w-full max-w-[1320px] flex-wrap gap-8 px-2 text-white font-bold text-lg">
-        {/* <a href="/files/lista-maternal2.pdf" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
+        <a href="/files/material-maternal2.doc" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
           Maternal II
         </a>
-        <a href="/files/lista-pre1.pdf" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
+        <a href="/files/material-pre1.doc" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
           Pré I
         </a>
-        <a href="/files/lista-pre2.pdf" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
+        <a href="/files/material-pre2.doc" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
           Pré II
         </a>
-        <a href="/files/lista-1ano.pdf" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
+        <a href="/files/material-1ano.docx" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
           1º ano
         </a>
-        <a href="/files/lista-2ano.pdf" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
+        <a href="/files/material-2ano.docx" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
           2º ano
         </a>
-        <a href="/files/lista-3ano.pdf" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
+        <a href="/files/material-3ano.docx" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
           3º ano
         </a>
-        <a href="/files/lista-4ano.pdf" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
+        <a href="/files/material-4ano.docx" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
           4º ano
         </a>
-        <a href="/files/lista-5ano.pdf" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
+        <a href="/files/material-5ano.docx" target="_blank" className="rounded-2xl h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px]">
           5º ano
         </a>
-        <a href="/files/lista-fundamental2-medio.pdf" target="_blank" className="rounded-2xl h-fit min-h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px] text-center ">
+        {/* <a href="/files/lista-fundamental2-medio.pdf" target="_blank" className="rounded-2xl h-fit min-h-10 px-4 bg-[#4f8e34] flex justify-center items-center w-full max-w-[340px] text-center ">
           Ensino Fundamental II e Ensino Médio
         </a> */}
       </div>
