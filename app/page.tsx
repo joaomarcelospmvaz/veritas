698ac5ed0fef3955966343f4f45e9c8372048b5c
João Vaz <joaomarcelospmvaz@gmail.com>
2026-09-11 10:46:37 -0300

    feat: changing banners and text bilingue

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,24 @@ import { Carrossel } from "@/components/Carrossel";
 import { Map } from "@/components/Map";
 import { Partners } from "@/components/Partners";
 
-import BannerImg1 from "@/public/assets/banner-site-veritas.jpg"
-import BannerImg2 from "@/public/assets/banner-site-veritas-1.jpg"
-import BannerImg3 from "@/public/assets/banner-aprovacao1.jpg"
-import BannerImg4 from "@/public/assets/banner-aprovacao2.jpg"
-
+import BannerImg1 from "@/public/assets/banner-home-1.jpg"
+import BannerImg2 from "@/public/assets/banner-home-2.jpg"
+import BannerImg3 from "@/public/assets/banner-home-3.jpg"
 
 
 export default function Home() {
   const mock = [
     {
-      title: 'banner aprovação 1',
-      url: BannerImg3.src,
+      title: 'banner volta as aulas',
+      url: BannerImg1.src,
     },
     {
-      title: 'banner aprovação 2',
-      url: BannerImg4.src,
-    },
-    {
-      title: 'editora do brasil',
+      title: 'banner veritas',
       url: BannerImg2.src,
     },
     {
-      title: 'transformação',
-      url: BannerImg1.src,
+      title: 'banner matrículas abertas',
+      url: BannerImg3.src,
     },
   ]
 
